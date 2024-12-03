6f457912ecd9ca338febdd19ec955c4c08e803d8
Bartek Archacki <bartekarchacki8@gmail.com>
2024-12-03 16:32:49 +0000

    Added blogs support

--- src/my_project_backend/src/blog.rs
+++ src/my_project_backend/src/blog.rs
@@ -1,5 +1,20 @@
-struct Blog{
-    title: String,
-    date: u32
+use candid::CandidType;
 
+#[derive(Clone, CandidType)]
+pub struct Blog{
+    title: String,
+    date: u32,
+    content: String,
+    tags: Vec<String>
+}
+
+impl Blog{
+    pub fn new(title: String, date: u32, content: String, tags: Vec<String>) -> Self {
+        Self {
+            title,
+            date, 
+            content,
+            tags
+        }
+    }
 }
--- src/my_project_backend/src/lib.rs
+++ src/my_project_backend/src/lib.rs
@@ -1,18 +1,21 @@
 use std::cell::RefCell;
 
+use crate::blog::Blog;
+
 mod blog;
 
 thread_local! {
-    static BLOGS: RefCell<Vec<String>> = RefCell::new(Vec::new());
+    static BLOGS: RefCell<Vec<Blog>> = RefCell::new(Vec::new());
 }
 
 #[ic_cdk::update]
-fn add_blog(new_blog: String) {
-    BLOGS.with(|blogs| blogs.borrow_mut().push(new_blog));
+fn add_blog(title: String, date: u32, content: String, tags: Vec<String>) {
+    let blog = Blog::new(title, date, content, tags);
+    BLOGS.with(|blogs| blogs.borrow_mut().push(blog));
 }
 
-#[ic_cdk::update]
-fn get_blogs() -> Vec<String>{
+#[ic_cdk::query]
+fn get_blogs() -> Vec<Blog>{
     BLOGS.with(|blogs| blogs.borrow().clone())
 }
 
