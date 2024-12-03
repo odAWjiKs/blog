b0838ceaf05ec585eb00f25c9a0ddde5acfd0891
Bartek Archacki <bartekarchacki8@gmail.com>
2024-12-03 15:37:31 +0000

    In progress to add blogs

--- src/my_project_backend/src/lib.rs
+++ src/my_project_backend/src/lib.rs
@@ -1,3 +1,21 @@
+use std::cell::RefCell;
+
+mod blog;
+
+thread_local! {
+    static BLOGS: RefCell<Vec<String>> = RefCell::new(Vec::new());
+}
+
+#[ic_cdk::update]
+fn add_blog(new_blog: String) {
+    BLOGS.with(|blogs| blogs.borrow_mut().push(new_blog));
+}
+
+#[ic_cdk::update]
+fn get_blogs() -> Vec<String>{
+    BLOGS.with(|blogs| blogs.borrow().clone())
+}
+
 #[ic_cdk::query]
 fn greet(name: String) -> String {
     format!("Hello, {}!", name)
